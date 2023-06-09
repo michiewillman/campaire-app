fa3f1055d3fe0aa17bbdef42be36ff845318b730
Michie <contact@michiewillman.com>
2023-06-08 20:21:31 -0700

    Get zipcode, Get AQI both working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,74 @@
-// fetch info from air quality api
+// Global variables
+var zipInput = $('#zip-input');
 
-var zip= '97209';
-var requestURL = "http://api.openweathermap.org/data/2.5/air_pollution?q=" + zip + "&appid={id_here}";
 
-fetch (requestURL)
-.then(function (response) {
-  if (response.ok) {
-    response.json().then(function (data) {
-      console.log("response is coming back ok");
-      console.log(data);
-    });
+// function to get local storageg and render last zipcode
+
+// Get coordinates for the user-inputted Zipcode
+function getCoords(zip) {
+  var coordsURL = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zip + "&appid=1cca1ff3113de5f3c3d237f233f7da56";
+
+  fetch (coordsURL)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        // console.log(data.lat, data.lon);
+        getAirQuality(data.lat, data.lon);
+      })
+    }
+  });
+}
+
+// Get the air quality of the Zipcode's coordinates
+function getAirQuality(latitude, longitude) {
+  var airQualityUrl = "http://api.openweathermap.org/data/2.5/air_pollution?lat=" + latitude + "&lon=" + longitude + "&appid=1cca1ff3113de5f3c3d237f233f7da56";
+
+  fetch (airQualityUrl)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data.list[0].main.aqi);
+        var aqi = data.list[0].main.aqi;
+        displayResults(aqi);
+        // save to local storage here ?
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  });
+}
+
+function displayResults(aqi) {   
+  var testDisplay = $('#current-air');
+
+  if (aqi >= 3) {
+    testDisplay.text("Current air quality in " + zipInput.val() + " is " + aqi + ". Go Camping!");
   } else {
-    alert('Error: ' + response.statusText);
+    testDisplay.text("Current air quality in " + zipInput.val() + " is " + aqi + ". Maybe not the best time to camp in this area.");
+    // Suggest other zipcodes to camp in ?
+    var otherZips = $('#nearby-zips');
+    otherZips.text("Try searching these closest zipcodes. They have a bit better air quality.");
   }
-});
+
+}
+
+// Searches the inputted Zipcode on search-button click
+function searchZipcode(event) {
+  event.preventDefault();
+  var zip = zipInput.val();
+
+  // If  Zipcode is inputted when button is clicked
+  if (zip) {
+    getCoords(zip);
+    // TODO: clear the input form
+  }
+
+  // TODO: function to save inputted zipcode to local storage
+}
+
+
+// function to fetch info from Google Maps api for nearby Campgrounds from a given zip code
+
 
 
 // if (airQuality is Good || Fair || Moderate) {
@@ -22,6 +77,7 @@ fetch (requestURL)
 //   then we display a message to not go camping
 // })
 
-// fetch info from Google Maps api for nearby Campgrounds from the given zip code
 
-// function get campgrounds from Google maps
+// Search button Event Listener
+var searchBtn = $('.search-button');
+searchBtn.on("click", searchZipcode);
