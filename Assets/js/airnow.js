ca6ccbbc2b4965cf86d66117ff3c810b4396ec1e
qwsargent <quinnwsargent@gmail.com>
2023-06-13 16:29:58 -0700

    made api key a global variable, removed all console.logs

--- Assets/js/airnow.js
+++ Assets/js/airnow.js
@@ -1,51 +1,51 @@
 var zipInput = $('#zip-input');
+var airnowAPI = ('8EBC362A-4EB7-4EC2-ABB1-FBA385B90C7D')
 
 function getCoordinates(zip) {
     //console.log();
-    var coordinatesUrl = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=" + zip + "&api_key=8EBC362A-4EB7-4EC2-ABB1-FBA385B90C7D";
-   console.log(coordinatesUrl)
+    var coordinatesUrl = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=" + zip + "&api_key=" + airnowAPI;
+  
     fetch (coordinatesUrl)
        // mode: "no-cors"
     .then(function (response) {
         
          if(response.ok) {
             response.json().then(function(data) {
-                console.log(data[0]);
+                
                  var Latitude = data[0].Latitude;
                  var Longitude = data[0].Longitude;
                  getAirQuality(Latitude, Longitude);
-                 console.log(data[0].Latitude);
-                console.log(data[0].Longitude);
-             })
+             
+               })
          }
     });
 }
 
 
 function getAirQuality(lat, long) {
-    var airQualityUrl = "https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=" + lat + "&longitude=" + long + "&distance=1000&api_key=8EBC362A-4EB7-4EC2-ABB1-FBA385B90C7D";
+    var airQualityUrl = "https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=" + lat + "&longitude=" + long + "&distance=1000&api_key=" + airnowAPI;
 
     fetch(airQualityUrl)
     .then(function (response) {
         if(response.ok) {
             response.json().then(function (data) {
                 var aqi = data[0].AQI;
-                console.log(data);
-                //var loc = data[0].ReportingArea;
-                //console.log(data[0].ReportingArea);
-                displayResults(aqi);
-                //displayResults(state);
+                
+                var loc = data[0].ReportingArea;
+                
+                displayResults(aqi, loc);
+                
             })
         }
     });
 }
 
-function displayResults(aqi, state) {
+function displayResults(aqi, loc) {
     var displayAQI = $('#current-air');
     if (aqi < 50) {
-        displayAQI.text("Your Air quality is " + aqi);
+        displayAQI.text("Your Air quality is " + aqi +" in " + loc);
     } else if (50 <= aqi > 100) {
-        displayAQI.text("Your Air quality is " + aqi);
+        displayAQI.text("Your Air quality is " + aqi + " in " + loc);
     }
     return;
 }
@@ -62,3 +62,4 @@ function searchZipcode(event) {
 
 var searchBtn = $('.search-button');
 searchBtn.on("click", searchZipcode);
+
