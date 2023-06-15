303a6cf48fffb8f721c4c2d1cf1ea0beea41fa11
qwsargent <quinnwsargent@gmail.com>
2023-06-14 18:58:53 -0700

    functioning api call for location and API for given location

--- Assets/js/airnow.js
+++ Assets/js/airnow.js
@@ -2,54 +2,46 @@ var zipInput = $('#zip-input');
 var latLongInput = $('#zip-input');
 var airnowAPI = ('8EBC362A-4EB7-4EC2-ABB1-FBA385B90C7D');
 var locationAPI = ('pk.89ed628237a7d3a065d576b871965ac0');
+var iQAirAPI = ('3ac59854-8742-4b4c-b4f1-4ea76e8f0301');
 
-// function getCoordinates(zip) {
-//     //console.log();
-//     var coordinatesUrl = "https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=" + zip + "&api_key=" + airnowAPI;
-  
-//     fetch (coordinatesUrl)
-//        // mode: "no-cors"
-//     .then(function (response) {
-        
-//          if(response.ok) {
-//             response.json().then(function(data) {
-                
-//                  var Latitude = data[0].Latitude;
-//                  var Longitude = data[0].Longitude;
-//                  getAirQuality(Latitude, Longitude);
-             
-//                })
-//          }
-//     });
-// }
 function getCoordinates(zip) {
-    var coordinatesURL = "https://us1.locationiq.com/v1/search?key=" + locationAPI + "&postalcode=" + zip + "&format=json";
-    // coordinatesURL = "https://us1.locationiq.com/v1/reverse?key=" + locationAPI + "&lat=42.1946&lon=122.7095&format=json";
-    // coordinatesURL = "https://us1.locationiq.com/v1/search?key=" + locationAPI + "&postalcode=98520&format=json";
+    var coordinatesURL = "https://us1.locationiq.com/v1/search?key=" + locationAPI + "&country=USA&postalcode=" + zip + "&format=json";
+    
+   
     fetch(coordinatesURL) 
     .then(function(response){
         console.log(response.status)
         if(response.ok) {
             response.json().then (function(data){
                 console.log(data);
+                var Latitude = data[0].lat;
+                var Longitude = data[0].lon;
+                console.log(Latitude, Longitude);
+                getAirQuality(Latitude, Longitude);
             })
         }
     });
 }
 
 
-function getAirQuality(lat, long) {
-    var airQualityUrl = "https://www.airnowapi.org/aq/forecast/latLong/?format=application/json&latitude=" + lat + "&longitude=" + long + "&distance=1000&api_key=" + airnowAPI;
-
+function getAirQuality(lat, lon) {
+    var  airQualityUrl = "http://api.airvisual.com/v2/nearest_city?lat=" + lat + "&lon=" + lon + "&key=" + iQAirAPI;
+    
     fetch(airQualityUrl)
     .then(function (response) {
+        console.log(response.status)
         if(response.ok) {
             response.json().then(function (data) {
-                var aqi = data[0].AQI;
+               
                 
-                var loc = data[0].ReportingArea;
                 
-                displayResults(aqi, loc);
+                console.log(data.data.current.pollution.aqius);
+
+                var aqi = data.data.current.pollution.aqius;
+                var loc = data.data.city
+
+                
+                displayResults(aqi,loc);
                 
             })
         }
@@ -74,14 +66,6 @@ function searchZipcode(event) {
         getCoordinates(zip);
     }
 }
-// function searchLatLong(event) {
-//     event.preventDefault();
-//     var latLong = latLongInput.val()
-
-//     if (latLong) {
-//         getAirQuality(latLong);
-//     }
-// }
 
 
 var searchBtn = $('.search-button');
