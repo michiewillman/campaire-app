acf87dde110fad0a4fabeaa5a3d6ae64483bf9df
Dusty Brigsby <108590519+dustybrigsby@users.noreply.github.com>
2023-06-14 21:04:09 -0700

    Update to index and script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,100 +2,97 @@
 var zipInput = $("#zip-input");
 var map;
 var googleMapId = "3ee7328d82b483e6";
-var zip = 97203;
+//var zip = 97203;
+var locationAPI = "pk.89ed628237a7d3a065d576b871965ac0";
+var iQAirAPI = "3ac59854-8742-4b4c-b4f1-4ea76e8f0301";
 
 // function to get local storage & render last 3-4 zipcodes
 
 // Get coordinates for the user-inputted Zipcode
-function getCoords(zip) {
-  var coordsURL =
-    "http://api.openweathermap.org/geo/1.0/zip?zip=" +
+function getCoordinates(zip) {
+  var coordinatesURL =
+    "https://us1.locationiq.com/v1/search?key=" +
+    locationAPI +
+    "&country=USA&postalcode=" +
     zip +
-    "&appid=1cca1ff3113de5f3c3d237f233f7da56";
+    "&format=json";
 
-  fetch(coordsURL).then(function (response) {
+  fetch(coordinatesURL).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
-        console.log(data.lat, data.lon);
-
-        getAirQuality(data.lat, data.lon);
+        var Latitude = data[0].lat;
+        var Longitude = data[0].lon;
+        getCampgrounds(Latitude, Longitude);
+        getAirQuality(Latitude, Longitude);
       });
     }
   });
 }
 
 // Get the air quality of the Zipcode's coordinates
-function getAirQuality(latitude, longitude) {
+function getAirQuality(lat, lon) {
   var airQualityUrl =
-    "http://api.openweathermap.org/data/2.5/air_pollution?lat=" +
-    latitude +
+    "http://api.airvisual.com/v2/nearest_city?lat=" +
+    lat +
     "&lon=" +
-    longitude +
-    "&appid=1cca1ff3113de5f3c3d237f233f7da56";
+    lon +
+    "&key=" +
+    iQAirAPI;
 
   fetch(airQualityUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        var aqi = data.list[0].main.aqi;
-
         console.log(data);
+        var aqi = data.data.current.pollution.aqius;
+        var city = data.data.city;
+        var state = data.data.state;
 
-        getCampgrounds(latitude, longitude);
-        displayResults(aqi);
-        // save to local storage here ?
+        displayResults(aqi, city, state);
       });
-    } else {
-      alert("Error: " + response.statusText);
     }
   });
 }
 
-function displayResults(aqi) {
-  var testDisplay = $("#current-air");
-
-  if (aqi <= 3) {
-    testDisplay.text(
-      "Current air quality in " +
-        zipInput.val() +
-        " is " +
-        aqi +
-        ". Go Camping!"
+function displayResults(aqi, city, state) {
+  var displayAQI = $("#current-air");
+  if (0 < aqi < 50) {
+    displayAQI.text(
+      "Your Air quality is " + aqi + " in " + city + ", " + state
     );
-  } else {
-    testDisplay.text(
-      "Current air quality in " +
-        zipInput.val() +
-        " is " +
-        aqi +
-        ". Maybe not the best time to camp in this area."
-    );
-    // Suggest other zipcodes to camp in ?
-    var otherZips = $("#nearby-zips");
-    otherZips.text(
-      "Try searching these closest zipcodes. They have a bit better air quality."
+  } else if (50 <= aqi > 100) {
+    displayAQI.text(
+      "Your Air quality is " + aqi + " in " + city + ", " + state
     );
   }
+  return;
 }
+// Suggest other zipcodes to camp in ?
+//     var otherZips = $("#nearby-zips");
+//     otherZips.text(
+//       "Try searching these closest zipcodes. They have a bit better air quality."
+//     );
+//   }
+// }
 
 // Searches the inputted Zipcode on search-button click
-function searchZipcode(event) {
-  // If user has nothing in local storage, show entry screen + hide results.
-  // If user has something in local storage, hide entry screen + show results.
 
+// If user has nothing in local storage, show entry screen + hide results.
+// If user has something in local storage, hide entry screen + show results.
+
+function searchZipcode(event) {
   event.preventDefault();
   var zip = zipInput.val();
 
-  console.log("zip: " + zip);
-
-  // If  Zipcode is inputted when button is clicked
   if (zip) {
-    getCoords(zip);
-    // TODO: clear the input form
+    getCoordinates(zip);
   }
-
-  // TODO: function to save inputted zipcode to local storage
 }
+
+// If  Zipcode is inputted when button is clicked
+
+// TODO: clear the input form
+
+// TODO: function to save inputted zipcode to local storage
 
 // function to fetch info from Google Maps api for nearby Campgrounds from a given zip code
 async function getCampgrounds(lat, lng) {
