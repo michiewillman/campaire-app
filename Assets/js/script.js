84c286d437e419783e824176be31eaf01fe260f0
Michie <contact@michiewillman.com>
2023-06-19 19:03:22 -0700

    Corrected script for maps

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
+
   // Global variables
   var map;
-  var service = new google.maps.places.PlacesService(map);
   var locationAPI = "pk.89ed628237a7d3a065d576b871965ac0";
   var iQAirAPI = "3ac59854-8742-4b4c-b4f1-4ea76e8f0301";
 
@@ -12,63 +11,62 @@ $(document).ready(function() {
     event.preventDefault();
     var input = $(this).closest('.search-form').find('.zip-input');
     var zip = input.val();
-  
+
     if (zip) {
-        getCoordinates(zip);
+      getCoordinates(zip);
     }
-  
+
     // Clear input field
     input.val('');
   }
 
   // Get coordinates for the user-inputted Zipcode
   function getCoordinates(zip) {
-    var coordinatesURL = "https://us1.locationiq.com/v1/search?key=" + locationAPI + "&country=USA&postalcode=" + zip + "&format=json";
-    
-  
-    fetch(coordinatesURL) 
-    .then(function(response){
-        
-        if(response.ok) {
-            var json = response.json();
-            return json;
-        }
-    }).then (function(data){
-                
-      var lat = data[0].lat;
-      var lng = data[0].lon;
-      var locDetails = data[0].display_name;
-      //getCampgrounds(lat, lng);
-      getAirQuality(lat, lng, locDetails);
-  });
+    var coordinatesURL =
+      "https://us1.locationiq.com/v1/search?key=" +
+      locationAPI +
+      "&country=USA&postalcode=" +
+      zip +
+      "&format=json";
+
+    fetch(coordinatesURL).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          var Latitude = data[ 0 ].lat;
+          var Longitude = data[ 0 ].lon;
+          initMap(Latitude, Longitude, zip);
+          getAirQuality(Latitude, Longitude);
+        });
+      }
+    });
   }
 
   // Get the air quality of the Zipcode's coordinates
   function getAirQuality(lat, lng, locDetails) {
-    var  airQualityUrl = "http://api.airvisual.com/v2/nearest_city?lat=" + lat + "&lon=" + lng + "&key=" + iQAirAPI;
-    
+    var airQualityUrl = "http://api.airvisual.com/v2/nearest_city?lat=" + lat + "&lon=" + lng + "&key=" + iQAirAPI;
+
     fetch(airQualityUrl)
-    .then(function (response) {
-        
-        if(response.ok) {
-            response.json().then(function (data) {
-                var aqi = data.data.current.pollution.aqius;
-                // var city = data.data.city;
-                // var state = data.data.state;
-                
-                
-                displayResults(aqi, locDetails);
-                
-            })
+      .then(function (response) {
+
+        if (response.ok) {
+          response.json().then(function (data) {
+            var aqi = data.data.current.pollution.aqius;
+            // var city = data.data.city;
+            // var state = data.data.state;
+
+
+            displayResults(aqi, locDetails);
+
+          })
         }
-    });
+      });
   }
 
   function displayResults(aqi, locDetails) {
     var currentContainer = $('#current-air');
     var resourcesContainer = $('#resources-container');
     var displayAQI = $('#display-aqi');
-    displayAQI.text("Your Air quality is " + aqi +" in " + locDetails);
+    displayAQI.text("Your Air quality is " + aqi + " in " + locDetails);
     if (aqi < 50) { // if air quality = good
       var positiveMessage = $('<div>');
       positiveMessage.addClass('ui floating positive message');
@@ -108,12 +106,8 @@ $(document).ready(function() {
 
   // function to fetch info from Google Maps api for nearby Campgrounds from a given zip code
   function initMap(lat, lng, zip) {
-    console.log("lat: " + lat);
-    console.log("lng: " + lng);
-
     var coords = new google.maps.LatLng(lat, lng);
-
-    console.log("coords: " + coords);
+    var radius = 15000;
 
     // The map, centered at searched zip
     map = new google.maps.Map(document.getElementById("campgrounds"), {
@@ -121,43 +115,72 @@ $(document).ready(function() {
       center: coords,
     });
 
+    var service = new google.maps.places.PlacesService(map);
     var nearbyCampgrounds = [];
 
     service.nearbySearch(
       {
         location: coords,
-        radius: 50000,
-        type: ["campground"],
+        radius: radius,
+        type: [ "campground" ],
       },
       (results, status) => {
-        console.log("results: " + results);
-
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           for (var i = 0; i < results.length; i++) {
-            console.log(results[i]);
-            nearbyCampgrounds.push(results[i]);
-            createMarker(results[i]);
+            // console.log(results[ i ]);
+            nearbyCampgrounds.push(results[ i ]);
+            createMarker(results[ i ], service);
           }
         }
       }
     );
   }
 
-  function createMarker(result) {
-    console.log("result: " + result);
+  function createMarker(result, service) {
+    // console.log("result: " + result);
+    var sizeScale = 20;
 
     var marker = new google.maps.Marker({
       map: map,
       position: result.geometry.location,
+      icon: { url: result.icon, scaledSize: new google.maps.Size(sizeScale, sizeScale) },
+      // label: result.name,
+      clickable: true,
     });
 
-    marker.setLabel(result.name + " ");
-    marker.setClickable(true);
+    var fullAddress = "unchanged";
+    var placeUrl = "unchanged";
+
+    service.getDetails({
+      placeId: result.place_id,
+      fields: [
+        "formatted_address",
+        "url"
+      ],
+    }, (results) => {
+      placeUrl = results.url;
+      fullAddress = results.formatted_address;
+
+
+      // console.log("placeUrl: " + placeUrl);
+      // console.log("fullAddress: " + fullAddress);
+
+      var infoWindow = new google.maps.InfoWindow({
+        content: "<a href='" + placeUrl + "' target=blank>" + result.name + "</a><br>" + result.vicinity,
+
+      });
+      // Add a click event listener to the marker
+      marker.addListener('click', function () {
+        // Open the info window when the marker is clicked
+        infoWindow.open(map, marker);
+      });
+    });
+
   }
 
-    // Search button Event Listener
-    $("#search-btn-1").on("click", searchZipcode);
-    $("#search-btn-2").on("click", searchZipcode);
-    initMap(45.5152, -122.6784, 97203);
+  // Search button Event Listener
+  $("#search-btn-1").on("click", searchZipcode);
+  $("#search-btn-2").on("click", searchZipcode);
+  initMap(45.5152, -122.6784, 97203);
 
 });
