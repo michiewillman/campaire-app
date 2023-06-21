a1d568050d56ebafec2174f610cb26b9b8c25795
Michie <contact@michiewillman.com>
2023-06-20 17:50:19 -0700

    Remove cruft/clean up script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -63,41 +63,34 @@ $(document).ready(function () {
   }
 
   function displayResults(aqi, locDetails) {
-    var searchResults = $('#search-results');
     var currentContainer = $('#current-air');
     var resourcesContainer = $('#resources-container');
-    var campgroundContainer = $('#campgrounds');
+    var campContainer = $('#camp-results');
+
     var displayAQI = $('#display-aqi');
     displayAQI.text("Your Air quality is " + aqi + " in " + locDetails);
     
     var resultMessage = $('#result-msg');
     resultMessage.removeClass();
-    campgroundContainer.removeClass();
+    campContainer.removeClass();
     resourcesContainer.removeClass();
     
-    var disclaimer = $('#disclaimer');
-    disclaimer.text("Please note: Map will only display camping within 50 kilometers.");
-
     if (aqi < 50) { // if air quality = good
       resourcesContainer.addClass('hide');
       // Add positive message styling 
       resultMessage.addClass('ui floating positive message');
       resultMessage.text('Pack your things, the air quality is good enough to camp!');
-      campgroundContainer.removeClass('hide');
-      // Add maps disclaimer
-      searchResults.append(disclaimer);
+      campContainer.removeClass('hide');
     } 
       else if (aqi > 50 && aqi < 150) { // if air quality = moderate
       resourcesContainer.addClass('hide');
       // Add warning message styling 
       resultMessage.addClass('ui floating warning message');
       resultMessage.text('Air quality is acceptable for camping. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.');
-      campgroundContainer.removeClass('hide');
-      // Add maps disclaimer
-      searchResults.append(disclaimer);
+      campContainer.removeClass('hide');
     } 
       else { // if air quality = unhealthy or worse
-      campgroundContainer.addClass('hide');
+      campContainer.addClass('hide');
       // Add negative message styling 
       resultMessage.addClass('ui floating negative message');
       resultMessage.text("The air quality around you is unhealthy for most. Maybe not the best time to camp. Curl up with a good book and minimize your time outside.");
